c2f19879bcd6798b5165ff6b8cb0a4f6c41dc624
Guess <ihylmfaoo@gmail.com>
2026-08-08 14:19:15 -0400

    Update kiu.js

--- kiu.js
+++ kiu.js
@@ -1,3 +1,8 @@
+const s = document.createElement("script");
+s.src = "https://cdn.jsdelivr.net/npm/lil-gui@0.20/+esm";
+s.type = "module";
+document.head.appendChild(s);
+
 import GUI from 'lil-gui';
 const gui = new GUI();
 const myObject = {
