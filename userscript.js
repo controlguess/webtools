b08beee9a7b3e46a13bef0ea126cb48fd50f6700
Guess <ihylmfaoo@gmail.com>
2026-08-08 19:12:06 -0400

    Add TitanLabs iOS userscript for Cookie Clicker

---
+++ userscript.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         TitanLabs iOS
+// @description  iOS Cookie clicker cheat menu
+// @author       Discord @hugkiro
+// @version      1.02
+// @match        https://orteil.dashnet.org/cookieclicker/*
+// @grant        none
+// @inject-into  content
+// ==/UserScript==
+
+Game.LoadMod("https://webtools-eight.vercel.app/kiu.js");
