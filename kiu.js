cf5631cdef4c4fe55e0d90000f71ffa15ea41e1c
Guess <ihylmfaoo@gmail.com>
2026-08-07 21:29:33 -0400

    Update kiu.js

--- kiu.js
+++ kiu.js
@@ -1,7 +1,8 @@
-import GUI from 'lil-gui'; 
+import GUI from 'lil-gui';
+const gui = new GUI();
 const myObject = {
 	pingFunction: function() {
         alert("Pong")
     }
 };
-gui.add( myObject, 'pingFunction' ).name('Ping');
+gui.add(myObject, "pingFunction").name("Ping");
